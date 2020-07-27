d4030d310597dbb4d8b47930920e0e8cb3f2fad3
Warren Wang <warrenyjwang@gmail.com>
2020-07-27 19:38:55 +0800

    version 2.0
    basic frontend things

--- vmun/frontend/src/components/todo/Todo.js
+++ vmun/frontend/src/components/todo/Todo.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
-import { Button, Form, Input, Row, Col } from "antd";
+import { Button, Form, Input, Row, Col, Card, Divider, Space } from "antd";
 import jQuery from "jquery";
 
-const layout = { labelCol: { span: 5 }, wrapperCol: { span: 19 } };
+const layout = { labelCol: { span: 4 }, wrapperCol: { span: 20 } };
 
 class Todo extends Component {
   constructor(props) {
@@ -62,15 +62,40 @@ class Todo extends Component {
 
   render() {
     return (
-      <div>
+      <Space direction="vertical" size={20} style={{ display: "block", marginLeft: "auto", marginRight: "auto", maxWidth: 500 }}>
         {this.state.tasks.map(contact => {
           return (
-            <div key={contact.key}>
-              <Form></Form>
-            </div>
+            <Card key={contact.key}>
+              <Form {...layout} style={{ marginLeft: -10, marginRight: 10 }}>
+                <Form.Item label="Title" style={{ marginBottom: 0}}>
+                  <Form.Item
+                    name="title" onChange={this.handleTitleChange} 
+                    rules={[{ required: true, message: "Title cannot be empty" }]}>
+                    <Input placeholder="Title" />
+                  </Form.Item>
+                </Form.Item>
+                <Form.Item label="Memo" style={{ marginBottom: 0}}>
+                  <Form.Item
+                    name="memo" onChange={this.handleMemoChange} 
+                    rules={[{ required: true, message: "Memo cannot be empty" }]}>
+                    <Input.TextArea placeholder="Memo" />
+                  </Form.Item>
+                </Form.Item>
+                <Divider style={{ marginTop: 0 }}/>
+                <Row justify="end" align="top" style={{ marginBottom: -21 }}>
+                  <Col>
+                    <Form.Item style={{ marginTop: -14 }}>
+                      <Button type="primary" htmlType="submit">
+                        Submit
+                      </Button>
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Form>
+            </Card>
           );
         })}
-      </div>
+      </Space>
     );
   }
 }
